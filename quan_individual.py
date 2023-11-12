28b05ac6498ea636774bbff53f835395ce6d40c1
yuanquanlorrie <148088877+yuanquanlorrie@users.noreply.github.com>
2023-11-12 18:41:26 +0100

    quan_individual.py

--- quan_individual.py
+++ quan_individual.py
@@ -1,7 +1,6 @@
 # Import necessary libraries
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 from datetime import datetime, timedelta
 import yfinance as yf
